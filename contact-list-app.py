2c891e99b983b66e64c23421b471ed7ba6a6794e
BenguCambay <123668329+BenguCambay@users.noreply.github.com>
2024-09-01 23:07:36 +0300

    Update contact-list-app.py

--- contact-list-app.py
+++ contact-list-app.py
@@ -21,11 +21,11 @@ connection = mysql.connect()
 connection.autocommit(True)
 cursor = connection.cursor()
 
-database_name = db_name.readline().strip()
+
 def init_contact_list_db():
     
     contact_table = """
-    CREATE TABLE IF NOT EXISTS {database_name}.contact_list(
+    CREATE TABLE IF NOT EXISTS db_name.readline().strip().contact_list(
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     number VARCHAR(100) NOT NULL,
